23743c0c601fb23217f929d14d447a0ed2b976ac
hannah_k <118167218+hannahpietersen@users.noreply.github.com>
2023-02-07 20:18:23 +0530

    Add files via upload

--- changing bg color/js/index.js
+++ changing bg color/js/index.js
@@ -1,1 +1,14 @@
+const button = document.querySelector("button")
+const body = document.querySelector("body")
+
+const colors = ['red', 'green', 'blue', 'yellow', 'pink', 'purple', 'orange','brown','tomato','silver','Tan']
+body.style.backgroundColor = 'Plum'
+
+button.addEventListener("click", changeBackground)
+
+function changeBackground() {
+    const colorIndex = parseInt(Math.random()*colors.length)
+    body.style.backgroundColor = colors[colorIndex]
+}
+
 
